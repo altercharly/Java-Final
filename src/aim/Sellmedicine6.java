760a78be12d98ae2b27b9bf4421b35d29d429d65
julianaiup <julianaiup@yahoo.com>
2018-02-28 15:12:18 -0300

    Update Sellmedicine6.java

--- src/aim/Sellmedicine6.java
+++ src/aim/Sellmedicine6.java
@@ -31,52 +31,37 @@ public class Sellmedicine6 extends HttpServlet {
         // TODO Auto-generated constructor stub
     	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     		// TODO Auto-generated method stub
-    		response.getWriter().append("Served at: ").append(request.getContextPath());
-    		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/sellmedicine6.jsp");
-            dispatcher.forward(request, response);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/sellmedicine6.jsp");
+		dispatcher.forward(request, response);
     	}
 
     	
     	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     		// TODO Auto-generated method stub
-    		//doGet(request, response);
-	
-    HttpSession mysession1= (HttpSession) request.getSession();
-    Patient patient= (Patient) mysession1.getAttribute("patient");
-    		
-	HttpSession mysession4= (HttpSession) request.getSession();
-	Professional prof= (Professional) mysession4.getAttribute("professional");
-	    
-	 HttpSession mysession2= (HttpSession) request.getSession();
-	 Medicine med= (Medicine) mysession2.getAttribute("medicine");
-	 
-	 HttpSession mysession3= (HttpSession) request.getSession();
-	 Item item= (Item) mysession3.getAttribute("itemsel");
-	 
-	 HttpSession mysession5= (HttpSession) request.getSession();
-	 int cantItems= (int) mysession5.getAttribute("cantItems");
-	 
-	 Controller ctrl = new Controller();
-	 double total = cantItems*(double)item.getprice();
-	 Prescription p = new Prescription();
-	 p.setidItem(item.getidItem());
-	 p.setidpatient(patient.getidPatient());
-	 p.setidproffesional(prof.getidProffesional());
-	 Calendar date = Calendar.getInstance();
-	 Date prescdate = date.getTime();
-	 p.setprescriptionDate(prescdate);
-	 p.setTotal(total);
-	 int idPrescription = ctrl.setPrescription(p);
-	 Prescription_Item pi = new Prescription_Item();
-	 pi.setCantItem(cantItems);
-	 pi.setIdItem(item.getidItem());
-	 pi.setidPrescription(idPrescription);
-	 ctrl.setPrescription_Item(pi);
-	 ctrl.updateCantItem(item, cantItems);
-	 
-	 RequestDispatcher despachadorr = request.getRequestDispatcher("sellmedicine7.jsp");
-     despachadorr.forward(request, response);
-	
-
+		HttpSession mysession = (HttpSession) request.getSession();
+		Patient patient = (Patient) mysession.getAttribute("patient");
+		Professional prof = (Professional) mysession.getAttribute("professional");
+		Medicine med = (Medicine) mysession.getAttribute("medicine");
+		Item item = (Item) mysession.getAttribute("itemsel");
+	 	int cantItems = (int) mysession.getAttribute("cantItems");
+		Controller ctrl = new Controller();
+		double total = cantItems*(double)item.getprice();
+		Prescription p = new Prescription();
+		p.setidItem(item.getidItem());
+		p.setidpatient(patient.getidPatient());
+		p.setidproffesional(prof.getidProffesional());
+		Calendar date = Calendar.getInstance();
+		Date prescdate = date.getTime();
+		p.setprescriptionDate(prescdate);
+		p.setTotal(total);
+		int idPrescription = ctrl.setPrescription(p);
+		Prescription_Item pi = new Prescription_Item();
+		pi.setCantItem(cantItems);
+		pi.setIdItem(item.getidItem());
+		pi.setidPrescription(idPrescription);
+		ctrl.setPrescription_Item(pi);
+		ctrl.updateCantItem(item, cantItems);
+		RequestDispatcher despachadorr = request.getRequestDispatcher("sellmedicine7.jsp");
+		despachadorr.forward(request, response);
 	}
 }
